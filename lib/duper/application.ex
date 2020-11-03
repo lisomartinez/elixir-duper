5eeadedfd66a6ae9babdde6121c9ad9aa5fe8428
Lisandro Martinez <lisandromartinez@gmail.com>
2020-11-03 18:48:20 -0300

    add PathFinder

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -7,6 +7,7 @@ defmodule Duper.Application do
   def start(_type, _args) do
     children = [
         Duper.Results,
+        { Duper.PathFinder, "."},
     ]
 
     opts = [strategy: :one_for_one, name: Duper.Supervisor]
