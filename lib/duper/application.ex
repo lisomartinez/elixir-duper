650f4e0ed5a09d13e161a75ff9ccdcd8bd3eaad3
Lisandro Martinez <lisandromartinez@gmail.com>
2020-11-03 19:10:20 -0300

    add WorkerSupervisor

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -8,9 +8,10 @@ defmodule Duper.Application do
     children = [
         Duper.Results,
         { Duper.PathFinder, "."},
+        Duper.WorkerSupervisor,
     ]
 
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
